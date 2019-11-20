116cbc0cd14e5e6659246a9d06a0c779edf653dc
Brian Dong <dongbr@oregonstate.edu>
2019-11-19 21:48:32 -0800

    updated home

---
+++ handlebars/public/searchbar.js
@@ -0,0 +1,15 @@
+/* This function gets the user's input on the search bar which we will use to search our DB with
+We have a toggle between "Food" and "Recipe". We store the user's choice in a variable called
+food_or_recipe which we will later use in search.js to deciper which table to search.
+*/
+function search_foods_or_recipes() {
+
+    var food_or_recipe = document.getElementsByName("food_or_recipe")[0].value;
+    var user_search_input = document.getElementsByName("user_input_search")[0].value;
+
+    console.log("Search Type and User Search Input \n");
+    console.log(food_or_recipe);
+    console.log(user_search_input);
+    //construct the URL and redirect to it
+    window.location = '/search/' + encodeURI(food_or_recipe + '_' + user_search_input);   
+}
--- handlebars/search.js
+++ handlebars/search.js
@@ -1,13 +1,81 @@
-//Search Page get and post requests
-module.exports = function(){
-    var express = require('express');
+module.exports = function() {
+
+	var express = require('express');
     var router = express.Router();
 
-	router.get('/', function(req,res,next){
-		
+	
+	function searchInput(req, res, mysql, context, complete){
+		var user_search = req.params.s;
+		if (user_search.substring(0,4) === "food")
+		{
+			user_search = user_search.substring(5);
+			var query = "SELECT * FROM foods WHERE foods.food_name LIKE " + mysql.pool.escape(user_search + '%');
+			mysql.pool.query(query, function(error, results, fields){
+				if(error){
+					res.write(JSON.stringify(error));
+					res.end();
+				}
+				console.log("Search Results");
+				console.log(results);
+				console.log('\n');
+	
+				context.search = results;
+				complete();
+	
+			});
+		}
+		else 
+		{
+			user_search = user_search.substring(7);
+			var query = "SELECT * FROM recipes WHERE recipes.recipe_name LIKE " + mysql.pool.escape(user_search + '%');
+			mysql.pool.query(query, function(error, results, fields){
+				if(error){
+					res.write(JSON.stringify(error));
+					res.end();
+				}
+				console.log("Search Results");
+				console.log(results);
+				console.log('\n');
+	
+				context.search = results;
+				complete();
+	
+			});
+		}
+
+	}
+	
+	
+
+	
+	
+	router.get('/', function(req,res){
+		var callbackCount = 0;
+		var context = {};
+		context.jsscripts = ["searchbar.js"];
+		var mysql = req.app.get('mysql');
+	
+
 		res.render('search');
+
+        
 		
 	});
-	
+
+	router.get('/:s', function(req,res){
+		var callbackCount = 0;
+		var context = {};
+		context.jsscripts = ["searchbar.js"];
+		var mysql = req.app.get('mysql');
+		searchInput (req, res, mysql, context, complete);
+		function complete(){
+			callbackCount++;
+			if(callbackCount >= 1){
+				res.render('search');
+			}
+
+		}
+		
+	});
 	return router;
 }();
