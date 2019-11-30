ca687db6cca960bb1efaf08f4b201767fc07753f
Brian-Dong <49142531+Brian-Dong@users.noreply.github.com>
2019-11-30 13:15:46 -0800

    Add files via upload

--- handlebars/public/searchbar.js
+++ handlebars/public/searchbar.js
@@ -11,15 +11,62 @@ function search_foods_or_recipes() {
     window.location = '/search/' + encodeURI(food_or_recipe) + '/' + encodeURI(user_search_input);   
 }
 
+// UI function for a button to hide the display table when the user wants to see the ingredients of a recipe
+function hideIngredientTable() {
+    document.getElementById("displayIngredientsDiv2").style.display = "none";
+    document.getElementById("displayIngredientsDiv").style.display = "none";
+}
 
-// Mount functions (through AJAX or JQUERY) to specific buttons
+// UI function for a button to hide the form when the user wants to update a recipe's properties
+function hideUpdateRecipeProp() {
+    document.getElementById("updateProperties").style.display = "none";
+    document.getElementById("updatePropertiesHideButton").style.display = "none";
+}
+
+// turns the Yes/No answers in the HTML to 1's and 0's for the Update Recipe button
+function parseYesNo (word) {
+    if (word === "Yes") {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+
+// Mount functions (through AJAX and/or JQUERY) to specific buttons
 $(document).ready(function(){
+
     $(".showIngredientsButton").click(function(){
 
-        // retrieve recipe id and redirect to it for route handler
-        window.location ='/search/' + $("#user_search_type").attr('value') + '/show/' + encodeURI($(this).attr('value')) + '/' + $("#user_search_text").attr('value');
+        var currentRow = this.parentNode.parentNode.rowIndex;
+        var button_name = document.getElementById("recipe_table").rows[currentRow].cells[9].innerText;
+        var id = $(this).attr('value');
 
-      });   
+        // retrieve recipe id and redirect to it for route handler
+        // window.location ='/search/' + $("#user_search_type").attr('value') + '/show/' + encodeURI($(this).attr('value')) + '/' + $("#user_search_text").attr('value');
+        $.ajax({
+            url: '/search/recipe/ingredients/' +  encodeURI(id),
+            type: 'GET',
+            success: function(result){
+        
+            var showIngredientSource = $("#showIngredientTemplate").html();
+
+            console.log(showIngredientSource);
+            console.log(result);
+
+            var template = Handlebars.compile(showIngredientSource);
+
+            $("#displayIngredientsDiv2").html(template(result));
+            document.getElementById("displayIngredientsDiv").style.display = "block";
+            document.getElementById("displayIngredientsDiv2").style.display = "block";
+
+            }
+        })
+        
+
+
+    });   
 
     // would like to change the window.location portion of this delete button
     $(".deleteRecipeButton").click(function(){
@@ -27,7 +74,7 @@ $(document).ready(function(){
             url: '/search/delete/recipe/' + encodeURI($(this).attr('value')),
             type: 'DELETE',
             success: function(result){
-                window.location = '/search/';
+                window.location.reload(true);
             }
         })
 
@@ -56,7 +103,7 @@ $(document).ready(function(){
         // Attach the recipe name to the DOM for that stylage
         var parent_node = document.getElementById("updateProperties");
         var child_node = document.getElementById("updatePropertiesForm");
-        var title = document.createElement("h3");
+        var title = document.createElement("h2");
         var title_text = document.createTextNode("Update " + recipeName + "'s Recipe Properties");
         title.style.textAlign = "center";
         
@@ -74,8 +121,8 @@ $(document).ready(function(){
         // so that our DB knows which recipe to update
         document.getElementById("sendRecipeUpdate").value = $(this).attr('value');
 
-        document.getElementById("updatePropertiesForm").style.display = "block";
-        document.getElementById("updatePropertiesForm").style.fontWeight = "normal";
+        document.getElementById("updatePropertiesHideButton").style.display = "block";
+        parent_node.style.display = "block";
     });
 
     // Create the sendRecipeUpdate function
@@ -93,5 +140,89 @@ $(document).ready(function(){
 
 
     });
+
+    // ADD FOOD w/ JQUERY AJAX
+    $("#addFoodButton").click(function(){
+        $.ajax({
+        
+            url:'/search/food',
+            type:'POST',
+            data: $('#addFoodForm').serialize(),
+            success: function(result){
+                window.location.reload(true);
+            }
+        })
+        
+    });
+
+
+     // ADD RECIPE w/ JQUERY AJAX
+     $("#addRecipeButton").click(function(){
+        $.ajax({
+        
+            url:'/search/recipe',
+            type:'POST',
+            data: $('#addRecipeForm').serialize(),
+            success: function(result){
+                window.location.reload(true);
+            }
+        })
+        
+    });
+
+
+    $(".updateIngredientsButton").click(function(){
+        var id = $(this).attr('value');
+        window.location = "/search/updateIngredients/" + encodeURI(id);
+        
+
+        // Tried to do AJAX but it wasn't working...
+        /*
+        var recipeData = {};
+
+        // Grab current row of the button that was clicked
+        var currentRow = this.parentNode.parentNode.rowIndex;
+        var currentRowOfTable = document.getElementById("recipe_table").rows[currentRow];
+
+        // formulate the data from the HTML table
+        var recipeName = currentRowOfTable.cells[0].innerHTML;
+        var recipeMeat = currentRowOfTable.cells[1].innerHTML;
+        var recipeDairy = currentRowOfTable.cells[2].innerHTML;
+        var recipeNuts = currentRowOfTable.cells[3].innerHTML;
+        var recipeShellfish = currentRowOfTable.cells[4].innerHTML;
+        var recipeCarbs = currentRowOfTable.cells[5].innerHTML;
+        var recipeAnimalProd = currentRowOfTable.cells[6].innerHTML;
+        var recipeGluten = currentRowOfTable.cells[7].innerHTML;
+        var recipeSoy = currentRowOfTable.cells[8].innerHTML;
+
+        recipeData.recipeName = recipeName;
+        recipeData.recipeMeat = recipeMeat;
+        recipeData.recipeDairy = recipeDairy;
+        recipeData.recipeNuts = recipeNuts;
+        recipeData.recipeShellfish = recipeShellfish;
+        recipeData.recipeCarbs = recipeCarbs;
+        recipeData.recipeAnimalProd = recipeAnimalProd;
+        recipeData.recipeGluten = recipeGluten;
+        recipeData.recipeSoy = recipeSoy;
+        recipeData.id = id;
+        console.log(recipeData);
+
+        $.ajax({
+        
+            url:'/search/updateIngredient/' + id,
+            type:'post',
+            data: recipeData,
+            success: function(result){
+                console.log("Results", result);
+                // window.location.href = "http://flip3.engr.oregonstate.edu:6750/updateIngredient/" + id;
+            }
+        })
+        */
+    });
     
+
+    $("#foodsForRecipeButton").click(function(){
+
+    });
+
 });
