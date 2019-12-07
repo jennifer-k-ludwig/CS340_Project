ccb4d5769ec9c246f75131ec57cb221b2aefd505
Brian-Dong <49142531+Brian-Dong@users.noreply.github.com>
2019-12-06 20:51:31 -0800

    added case where user goes to the base url

--- handlebars/main.js
+++ handlebars/main.js
@@ -20,7 +20,8 @@ app.use('/search', require('./search.js'));
 app.use('/home', require('./home.js'));
 app.use('/user', require('./user.js'));
 app.use('/new_user', require('./new_user.js'));
-app.use('', require('./login.js'));
+app.use('', require('./home.js'));
+
 app.use(function(req,res){
   res.status(404);
   res.render('404');
